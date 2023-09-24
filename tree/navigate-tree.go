eae7e3ae7d47c055d7ad18975baf85a019278c42
ShayeGun <shayegan.7200@gmail.com>
2023-09-24 10:16:59 +0330

    complete binary tree recognition func

--- tree/navigate-tree.go
+++ tree/navigate-tree.go
@@ -79,17 +79,36 @@ func (n *Node) depthFullBinaryTree() int {
 	return depth - 1
 }
 
+func (n *Node) isCompleteBinaryTree(index int, numNodes int) bool {
+	if n == nil {
+		return true
+	}
+	// for complete binary tree we consider that the tree is complete until it shows that it is not -->
+	// --> we can realize this by comparing it to an array indexes cuz we can show complete binary trees in array form n, n*2+1, n*2+2
+	if index > numNodes {
+		return false
+	}
+	return n.lChild.isCompleteBinaryTree(index * 2 + 1, numNodes) && n.rChild.isCompleteBinaryTree(index * 2 + 2, numNodes)
+}
+
+func (n *Node) nodeCount() int {
+	if n == nil {
+		return 0
+	}
+	return (1 + n.lChild.nodeCount() + n.rChild.nodeCount()) 
+}
+
 func Printy(){
 	node := Node{2, &Node{root:3}, &Node{root:4}}
 	// TEST for perfect tree --> returns true
-	node.lChild = &Node{5, &Node{root:6}, &Node{root:7}}
-	node.rChild = &Node{5, &Node{root:6}, &Node{root:7}}
-	node.lChild.lChild = &Node{5, &Node{root:6}, &Node{root:7}}
-	node.lChild.rChild= &Node{5, &Node{root:6}, &Node{root:7}}
+	// node.lChild = &Node{5, &Node{root:6}, &Node{root:7}}
+	// node.rChild = &Node{5, &Node{root:6}, &Node{root:7}}
+	// node.lChild.lChild = &Node{5, &Node{root:6}, &Node{root:7}}
+	// node.lChild.rChild= &Node{5, &Node{root:6}, &Node{root:7}}
 
 
 	// TEST for full tree
 	// node.lChild.lChild = &Node{root:5,rChild: &Node{root:6}}
 	// node.rChild.rChild = &Node{7,&Node{root:8}, &Node{root:9}}
-	fmt.Println(node.isPrefectBinaryTree())
+	fmt.Println(node.isCompleteBinaryTree(0,node.nodeCount()))
 }
