066b5461652a86a7034d365304eb4f36726171fd
ShayeGun <shayegan.7200@gmail.com>
2023-09-29 20:48:09 +0330

    implementing delete node in avl tree :not finished

--- tree/avl/avl-tree.go
+++ tree/avl/avl-tree.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-type Node struct{
-	root int
+type Node struct {
+	root   int
 	lChild *Node
 	rChild *Node
 	height int
 }
 
-func (n *Node) getHeight() int{
+func (n *Node) getHeight() int {
 	if n == nil {
 		return 0
 	}
@@ -19,7 +19,7 @@ func (n *Node) getHeight() int{
 	return n.height
 }
 
-func (n *Node) rightRotation() *Node{
+func (n *Node) rightRotation() *Node {
 	x := n.lChild
 	temp := x.rChild
 	x.rChild = n
@@ -30,20 +30,20 @@ func (n *Node) rightRotation() *Node{
 	return x
 }
 
-func (n *Node) leftRotation() *Node{
+func (n *Node) leftRotation() *Node {
 	x := n.rChild
 	temp := x.lChild
 	x.lChild = n
-	n.rChild = temp 
+	n.rChild = temp
 	n.height = max(n.lChild.getHeight(), n.rChild.getHeight())
 	x.height = max(x.lChild.getHeight(), x.rChild.getHeight()) + 1
 	*n = *x
 	return x
 }
 
-func (n *Node) getBalance() int{
+func (n *Node) getBalance() int {
 	if n == nil {
-		return 0 
+		return 0
 	}
 	return n.lChild.getHeight() - n.rChild.getHeight()
 }
@@ -69,14 +69,14 @@ func (n *Node) insertNode(value int) *Node {
 		return n
 	}
 	// Update the balance factor of each node
-    // And, balance the tree
+	// And, balance the tree
 	n.height = max(n.lChild.getHeight(), n.rChild.getHeight()) + 1
 
 	// bf = balance factor
 	bf := n.getBalance()
 	if bf > 1 {
 		if value < n.lChild.root {
-			return  n.rightRotation()
+			return n.rightRotation()
 		} else if value > n.lChild.root {
 			n.lChild.leftRotation()
 			return n.rightRotation()
@@ -85,7 +85,7 @@ func (n *Node) insertNode(value int) *Node {
 
 	if bf < -1 {
 		if value > n.rChild.root {
-			return  n.leftRotation()
+			return n.leftRotation()
 		} else if value < n.rChild.root {
 			n.rChild.rightRotation()
 			n.leftRotation()
@@ -96,8 +96,49 @@ func (n *Node) insertNode(value int) *Node {
 	return n
 }
 
-// TODO: THERE IS A PROBLEM WITH HEIGHT IT'S WORKING BUT MY GUTS SAYS IT HAS SOME PROBLEMS :| 
-func Printy(){
+func (n *Node) nodeWithMinimumValue() *Node {
+	currentNode := n
+	for currentNode.lChild != nil {
+		currentNode = currentNode.lChild
+	}
+	return currentNode
+}
+
+func (n *Node) deleteNode(value int) *Node {
+	if n == nil {
+		return nil
+	}
+	if value > n.root {
+		n.rChild.deleteNode(value)
+	} else if value < n.root {
+		n.lChild.deleteNode(value)
+	} else {
+		if n.lChild == nil || n.rChild == nil {
+			var temp *Node
+			if n.lChild == nil {
+				temp = n.rChild
+			} else {
+				temp = n.lChild
+			}
+			// IF BOTH NODES WERE NIL
+			if temp == nil {
+				temp = n
+				n = nil
+			}
+		} else {
+			temp := n.nodeWithMinimumValue()
+			n.root = temp.root
+			n.rChild.deleteNode(temp.root)
+		}
+		if n == nil {
+			return nil
+		}
+	}
+	return n
+}
+
+// TODO: THERE IS A PROBLEM WITH HEIGHT IT'S WORKING BUT MY GUTS SAYS IT HAS SOME PROBLEMS :|
+func Printy() {
 	node := &Node{}
 	node.insertNode(4)
 	node.insertNode(5)
